e80fa4c53a2b566897e03c0ef3355a5a13d6f321
Sway007 <yanshiwei.ysw@antgroup.com>
2023-06-09 14:30:13 +0800

    feat: update readme

--- client/src/commands/template.ts
+++ client/src/commands/template.ts
@@ -28,8 +28,6 @@ const content = `Shader "Water" {
       VertexShader = vert;
       FragmentShader = frag;
 
-    #include <common>
-
     //   vec4 linearToGamma(vec4 linearIn) {
     //       return vec4(pow(linearIn.rgb, vec3(1.0 / 2.2)), linearIn.a);
     // }
